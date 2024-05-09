3a9b5ffd22aed6932bfe67c24b0536e7954b9c39
Scott Parrish <slparrish@gmail.com>
2024-05-09 10:55:54 -0400

    changes to data structure

--- Mobile Resume/Mobile Resume/Resume.swift
+++ Mobile Resume/Mobile Resume/Resume.swift
@@ -9,47 +9,53 @@ import Foundation
 
 
 struct Resume {
-    var contact: Contact
-    var jobs: Jobs
-    var education: [Education]
-    var skills: [Skills]
-}
-
-
-struct Contact {
     var fName: String
     var lName: String
     var address: String
     var phone: String
     var email: String
+    var jobs: [String : String]
+    var education: [String]
+    var skills: [Skill]
+    
+    init(fName: String, lName: String, address: String, phone: String, email: String, jobs: [String:String], education: [String], skills: [Skill]) {
+        self.fName = fName
+        self.lName = lName
+        self.address = address
+        self.phone = phone
+        self.email = email
+        self.jobs = jobs
+        self.education = education
+        self.skills = skills
+    }
 }
 
 
-struct Jobs {
-    var job: [Job]
-}
+//struct Job {
+//    var Employer: String
+//    var Description: String
+//}
 
-struct Job {
-    var Employer: String
-    var Description: String
-}
+//struct School {
+//    var schoolname: String
+//}
 
-struct Education {
-    var school: [School]
-}
-
-
-struct School {
-    var schoolname: String
-}
-
-struct Skills {
-    var skill: [Skill]
-}
+//struct Skills {
+//    var skill: [Skill]
+//    
+//    init(skill: [Skill]) {
+//        self.skill = skill
+//    }
+//}
 
 struct Skill {
     let skillname: String
     var skillType: SkillType
+    
+    init(skillname: String, skillType: SkillType) {
+        self.skillname = skillname
+        self.skillType = skillType
+    }
 }
 
 enum SkillType {
--- Mobile Resume/Mobile Resume/ViewController.swift
+++ Mobile Resume/Mobile Resume/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     var mySkill = Skill(skillname: "Python", skillType: .progLanguage)
     var mySkill2 = Skill(skillname: "Project Management", skillType: .management)
+    var mySkills = [mySkill, mySkill2]
+    var mySchools = ["East-West High School"]
+    var myJobs = ["McDonalds" : "Dishwasher"]
+    
+    var myResume = Resume(fName: "Steve", lName: "Jobs", address: "Infinite Loop, Cupertino, CA 11111", phone: "555-555-5555", email: "sjobs@example.com", jobs: myJobs, education: mySchools, skills: mySkills)
    
     override func viewDidLoad() {
         super.viewDidLoad()
